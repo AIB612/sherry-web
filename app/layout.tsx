89a005c7ca0e425ecdb01b1c85a69bae92d9ae07
sherryjjj <xuexuets@foxmail.com>
2026-03-24 16:44:53 +0100

    feat: add B favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,9 @@ export const metadata = {
     index: true,
   },
   icons: {
-    icon: '/logos/adobe.png',
-    shortcut: '/logos/adobe.png',
-    apple: '/logos/adobe.png',
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
   },
 };
 
