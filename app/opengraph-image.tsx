47b369f6bfee5a30366114437bd74fed66a58b19
sherryjjj <xuexuets@foxmail.com>
2026-04-07 17:20:50 +0200

    Fix: enable static export and update node version

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,4 +1,5 @@
 import OpengraphImage from "components/opengraph-image";
+export const dynamic = 'force-static'; // 👈 加上这一行，强制它静态化
 
 export default async function Image() {
   return await OpengraphImage();
